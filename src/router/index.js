4ad6f46af8f7fc6a33d107729bd92ac8b6799974
GeekYao <15801079691@163.com>
2018-03-06 13:44:15 +0800

    study hard and commit

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(Router)
 
 // 建议使用这种方式引用，打包的时候会分块打包，这样再前端页面会按需加载，不会一次性加载全部，定义变量的时候建议使用驼峰，首字母大写
 const Hello = (resolve) => {
-  import('components/hello/hello').then((module) => {
+  import('components/header').then((module) => {
     resolve(module)
   })
 }
